3856dd090c6a21a88f8f138cca59c85d51f0c399
Calix Huang <calix.huang1@gmail.com>
2020-02-10 10:30:01 -0800

    added cookies

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template lang="html">
   <div>
-    <navbar v-bind:loggedIn="loggedIn" @update:loggedIn="updateLoggedIn" @update:loggedInUser="updateLoggedInUser"></navbar>
-    <router-view v-bind:loggedInUser="loggedInUser" v-bind:loggedIn="loggedIn" @update:loggedIn="updateLoggedIn" @update:loggedInUser="updateLoggedInUser"></router-view>
+    <navbar v-bind:loggedIn="loggedIn"
+            @update:loggedIn="updateLoggedIn"
+            @update:loggedInUser="updateLoggedInUser"
+    />
+    <router-view v-bind:loggedInUser="loggedInUser"
+                 v-bind:loggedIn="loggedIn"
+                 @update:loggedIn="updateLoggedIn"
+                 @update:loggedInUser="updateLoggedInUser"
+    />
   </div>
 </template>
 
@@ -14,20 +21,30 @@ export default {
   },
   data() {
     return {
-      loggedIn: false,
-      loggedInUser: {},
+      loggedIn: ($cookies.get("loggedIn") == "true"), // Cookies return loggedIn as string, so this is converting to boolean
+      loggedInUser: $cookies.get("loggedInUser"),
     }
   },
   methods: {
     // Will update login with whatever value is provided -> used for login/logout/register
     updateLoggedIn: function(value) {
-      this.loggedIn = value;
+      $cookies.set("loggedIn", value); // Updating cookies
+      this.loggedIn = value; // Updating props
     },
     // Will update current logged in user with corresponding data
     updateLoggedInUser: function(value) {
-      this.loggedInUser = value;
+      $cookies.set("loggedInUser", value); // Updating cookies
+      this.loggedInUser = value; // Updating props
     },
-  }
+  },
+  // computed: {
+  //   loggedIn: () => {
+  //     return ($cookies.get("loggedIn") == "true");
+  //   },
+  //   loggedInUser: () => {
+  //     return $cookies.get("loggedInUser");
+  //   }
+  // }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -2,10 +2,15 @@ import Vue from 'vue';
 import App from './App.vue';
 import VueResource from 'vue-resource';
 import VueRouter from 'vue-router';
+import VueCookies from 'vue-cookies';
 import Routes from './routes.js'
 
 Vue.use(VueResource)
 Vue.use(VueRouter)
+Vue.use(VueCookies);
+
+// Setting global expiration time for cookies
+Vue.$cookies.config("1d");
 
 export const bus = new Vue();
 
